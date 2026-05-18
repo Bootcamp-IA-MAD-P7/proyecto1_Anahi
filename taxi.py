72394c05f02f10b5095f9fc1710f309fca68ee46
anahi-am <anahiamerisea@gmail.com>
2026-05-18 22:46:22 +0200

    feat: make rates configurable in class

--- taxi.py
+++ taxi.py
@@ -1,17 +1,15 @@
-RATE_MOVING = 0.05
-RATE_STOPPED = 0.02
-
 class Taxi:
     def __init__(self):
         self.fare = 0.00
         self.journey_active = False
         self.moving = False
+        self.rate_moving = 0.05
+        self.rate_stopped = 0.02
     
     def start_journey(self):
         self.fare = 0.00
         self.journey_active = True
         self.moving = True
-
 
     def end_journey(self):
         self.journey_active = False
@@ -25,6 +23,12 @@ class Taxi:
 
     def taximeter(self):
         if self.moving:
-            self.fare += RATE_MOVING
+            self.fare += self.rate_moving
         else:
-            self.fare += RATE_STOPPED
+            self.fare += self.rate_stopped
+    
+    def set_rates(self, rate_moving, rate_stopped):
+        if rate_moving <= 0 or rate_stopped <= 0:
+            raise ValueError("Las tarifas deben ser números positivos")
+    self.rate_moving = rate_moving
+    self.rate_stopped = rate_stopped
